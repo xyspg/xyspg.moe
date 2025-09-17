f577b0b6c00bf472c9fc674fb76ca7ef2b0e6345
Kang Jiaming <jiaming_kang@outlook.com>
2025-09-17 15:34:39 -0400

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,8 @@ const config = {
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
+        "simple-import-sort/imports": "warn",
+        
       },
     },
   ],
